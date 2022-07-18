f380d1c3d9538d2638a2ec93d953f1c2df440d43
эмиль ахметханов <emil.ah@yandex.ru>
2022-07-18 19:47:00 +0300

    Добавьте файлы проекта.

---
+++ Task7(again)/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_again_
+{
+    class Program
+    {
+        static double GetSquare(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону а  первого треугольника");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите сторону b  первого треугольника");
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите сторону c  первого треугольника");
+            double c1 = Convert.ToDouble(Console.ReadLine());
+            double s1 = GetSquare(a1, b1, c1);
+            Console.WriteLine("Введите сторону а  второго треугольника");
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите сторону b  второго треугольника");
+            double b2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите сторону c  второго треугольника");
+            double c2 = Convert.ToDouble(Console.ReadLine());
+            double s2 = GetSquare(a2, b2, c2);
+            Console.WriteLine("Площадь  первого треугольника  равна {0}", s1);
+            Console.WriteLine("Площадь  второго треугольника  равна {0}", s2);
+            if (s1 > s2)
+            {
+                Console.WriteLine("Площадь 1 треугольника больше");
+            }
+            else
+                if (s1 < s2)
+            {
+                Console.WriteLine("Площадь 2 треугольника больше");
+            }
+            else
+                Console.WriteLine("Площади треугольников равны");
+            Console.ReadKey();
+        }
+        
+        
+    }
+}
---
+++ Task7(again)2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_again_2
+{
+    class Program
+    {
+        static void GetParamCube(double a, out double s, out double v)
+        {
+            s = 6.0 * a * a;
+            v = a * a * a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double s;
+            double v;
+            GetParamCube(a, out s, out v);
+            Console.WriteLine("Площадь поверхности куба равна {0:.00}, Объем куба равен {1:.00}", s, v);
+            Console.ReadKey();
+        
+        }
+    }
+}
